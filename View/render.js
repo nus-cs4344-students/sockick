bc989c74092805d721c2dc040c37d17657fc90e4
Liu Tuo <liutuo098@gmail.com>
2015-03-30 23:33:55 +0800

    add bonus

--- View/render.js
+++ View/render.js
@@ -46,6 +46,67 @@ function Render() {
 		// wasd.scaleX = 0.5;
 		// wasd.scaleY = 0.5;
 		// this.scoreBoard.addChild(wasd);
+		
+		// bonus list
+		// freeze
+		var circleBlue = new createjs.Shape();
+		circleBlue.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 8);
+		circleBlue.x = 10;
+		circleBlue.y = 10;
+		this.scoreBoard.addChild(circleBlue);
+
+		var text_freeze = new createjs.Text("Freeze", "16px Comic Sans MS", "#000000");
+		text_freeze.x = 30;
+		text_freeze.y = -2;
+		this.scoreBoard.addChild(text_freeze);
+
+		// speed up
+		var circleBlue = new createjs.Shape();
+		circleBlue.graphics.beginFill("Red").drawCircle(0, 0, 8);
+		circleBlue.x = 10;
+		circleBlue.y = 30;
+		this.scoreBoard.addChild(circleBlue);
+
+		var text_speedup = new createjs.Text("Speed Up", "16px Comic Sans MS", "#000000");
+		text_speedup.x = 30;
+		text_speedup.y = 18;
+		this.scoreBoard.addChild(text_speedup);
+
+		// weight up
+		var circleBlue = new createjs.Shape();
+		circleBlue.graphics.beginFill("Yellow").drawCircle(0, 0, 8);
+		circleBlue.x = 10;
+		circleBlue.y = 50;
+		this.scoreBoard.addChild(circleBlue);
+
+		var text_weightup = new createjs.Text("Weight Up", "16px Comic Sans MS", "#000000");
+		text_weightup.x = 30;
+		text_weightup.y = 38;
+		this.scoreBoard.addChild(text_weightup);
+
+		// skill refresh
+		var circleBlue = new createjs.Shape();
+		circleBlue.graphics.beginFill("Green").drawCircle(0, 0, 8);
+		circleBlue.x = 10;
+		circleBlue.y = 70;
+		this.scoreBoard.addChild(circleBlue);
+
+		var text_refresh = new createjs.Text("Refresh", "16px Comic Sans MS", "#000000");
+		text_refresh.x = 30;
+		text_refresh.y = 58;
+		this.scoreBoard.addChild(text_refresh);
+
+		// reverse
+		var circleBlue = new createjs.Shape();
+		circleBlue.graphics.beginFill("Purple").drawCircle(0, 0, 8);
+		circleBlue.x = 10;
+		circleBlue.y = 90;
+		this.scoreBoard.addChild(circleBlue);
+
+		var text_reverse = new createjs.Text("Reverse", "16px Comic Sans MS", "#000000");
+		text_reverse.x = 30;
+		text_reverse.y = 78;
+		this.scoreBoard.addChild(text_reverse);
 
 		// start to tick
 		createjs.Ticker.addEventListener("tick", this.handleTick);
